bb9247257ad26c447f8b44675e8eac1a172b0aee
sam <semen.tinchurin@mail.ru>
2022-04-22 18:51:42 +0300

    connected some of the functions with Gui

--- main.py
+++ main.py
@@ -4,16 +4,6 @@ path = 'Notes'
 NOTE = 'Note №'
 
 
-# creating a new note
-def NewNote(number_of_note=1):
-    file_name = f'{NOTE} {number_of_note}.txt'
-    text = input('Enter a note: ')
-    if file_name in os.listdir(path):
-        file_name = f"{NOTE} {number_of_note + len(os.listdir(path))}.txt"
-    with open(f'Notes/{file_name}', 'w', encoding='utf-8') as file:
-        file.write(text)
-
-
 # editing an existing note
 def EditNote():
     list_of_notes = ListOfNotes()
@@ -29,14 +19,9 @@ def EditNote():
 # get a list of existing notes
 def ListOfNotes():
     list_of_notes = os.listdir(path)
-    print('Created notes:')
-    for item in list_of_notes:
-        print(item)
     return list_of_notes
 
 
-# delete existing note
-def DeleteNote():
-    choice = input('Enter a number of note to delete this note: ')
-    os.remove(f'{path}/{NOTE} {choice}.txt')
-    ListOfNotes()
+
+
+# NewNote()
--- pyqt_test.py
+++ pyqt_test.py
@@ -1,6 +1,7 @@
-from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QWidget, QPushButton
+from PyQt5 import QtWidgets, uic, QtGui, QtCore
+from PyQt5.QtWidgets import QWidget
 import sys
+
 from main import *
 
 
@@ -26,22 +27,61 @@ from main import *
 
 class App(QWidget):
     def __init__(self):
+        super().__init__()
         self.ui = None
+        self.notes_list = ListOfNotes()
+        self.model = QtGui.QStandardItemModel(self)
         self.start()
 
     def start(self):
         self.ui = uic.loadUi('notewidget_test.ui')
         self.ui.show()
         self.set()
+        self.setuplistveiw()
+
+    def setuplistveiw(self):
+        for note in self.notes_list:
+            item = QtGui.QStandardItem(note)
+            item.setData(note)
+            self.model.appendRow(item)
+        self.ui.listView.setModel(self.model)
+        self.ui.listView.setMouseTracking(True)
 
     def set(self):
-        self.ui.btn1.clicked.connect(lambda: NewNote(number_of_note=1))
-        self.ui.btn2.clicked.connect(lambda: self.check(btn=self.ui.btn2))
-        self.ui.btn3.clicked.connect(lambda: DeleteNote())
+        self.ui.btn1.clicked.connect(lambda: self.NewNote(number_of_note=1))
+        # self.ui.btn2.clicked.connect(lambda: self.check(btn=self.ui.btn2))
+        self.ui.btn3.clicked.connect(lambda: self.DeleteNote())
+        self.ui.listView.clicked.connect(lambda: self.item_clicked())
 
-    def check(self, btn):
-        print('Clicked button', btn)
+    # получил имя выбранного файла и его содержимое
+    @QtCore.pyqtSlot()
+    def item_clicked(self):
+        index = self.ui.listView.selectionModel().currentIndex()
+        file_name = index.data()
+        print(file_name)
+        with open(f"Notes/{file_name}", 'r', encoding='utf-8') as file:
+            file = file.read()
+            print(file)
 
+    # creating a new note
+    def NewNote(self, number_of_note=1):
+        file_name = f'{NOTE} {number_of_note}.txt'
+        text = self.ui.textEdit.toPlainText()
+        if file_name in os.listdir(path):
+            file_name = f"{NOTE} {number_of_note + len(os.listdir(path))}.txt"
+        with open(f'Notes/{file_name}', 'w', encoding='utf-8') as file:
+            file.write(text)
+
+        # update list of notes in ListView widget
+        item = QtGui.QStandardItem(file_name)
+        item.setData(file_name)
+        self.model.appendRow(item)
+
+    # delete existing note
+    def DeleteNote(self):
+        choice = input('Enter a number of note to delete this note: ')
+        os.remove(f'{path}/{NOTE} {choice}.txt')
+        ListOfNotes()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
